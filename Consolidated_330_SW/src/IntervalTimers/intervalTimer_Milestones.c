104d64a165d4bf0933f540d3d8124d7e9b2749c4
Luke Hsiao <lukehsiao@byu.edu>
2015-05-01 12:13:48 -0600

    Update and rename intervalTimer_Milestone1.c to intervalTimer_Milestones.c

--- Consolidated_330_SW/src/IntervalTimers/intervalTimer_Milestones.c
+++ Consolidated_330_SW/src/IntervalTimers/intervalTimer_Milestones.c
@@ -1,5 +1,6 @@
 #include "intervalTimer.h"
 #include "xil_io.h"
+#include "buttons.h"
 #include <stdio.h>
 
 #define TCR0_OFFSET 0x08
@@ -19,7 +20,13 @@ void waitALongTime() {
       a++;
 }
 
-int main() {
+void testAll() {
+  intervalTimer_initAll();
+  intervalTimer_resetAll();
+  intervalTimer_testAll();
+}
+
+void milestone1() {
   intervalTimer_init(0);
   intervalTimer_reset(0);
   // Show that the timer is reset.
@@ -38,4 +45,37 @@ int main() {
   waitALongTime();
   printf("timer_0 TCR0 value after wait:%lx\n\r", readTimer1Register(TCR0_OFFSET));
   printf("timer_0 TCR1 should have changed at this point:%ld\n\r", readTimer1Register(TCR1_OFFSET));
+}
+
+void milestone2() {
+  double duration0, duration1, duration2;
+  buttons_init();
+  intervalTimer_initAll();
+  intervalTimer_resetAll();
+  // Poll the push-buttons waiting for BTN0 to be pushed.
+  printf("Interval Timer Accuracy Test\n\r");
+  printf("waiting until BTN0 is pressed.\n\r");
+  while (!(buttons_read() & 0x1));
+  intervalTimer_start(0);
+  intervalTimer_start(1);
+  intervalTimer_start(2);
+  printf("started timers.\n\r");
+  printf("waiting until BTN1 is pressed.\n\r");
+  while (!(buttons_read() & 0x2));
+  intervalTimer_stop(0);
+  intervalTimer_stop(1);
+  intervalTimer_stop(2);
+  printf("stopped timers.\n\r");
+  intervalTimer_getTotalDurationInSeconds(0, &duration0);
+  intervalTimer_getTotalDurationInSeconds(1, &duration1);
+  intervalTimer_getTotalDurationInSeconds(2, &duration2);
+  printf("Time Duration 0: %6.2e seconds.\n\r", duration0);
+  printf("Time Duration 1: %6.2e seconds.\n\r", duration1);
+  printf("Time Duration 2: %6.2e seconds.\n\r", duration2);
+}
+
+int main() {
+  //milestone1();
+  //milestone2();
+  testAll();
 }
