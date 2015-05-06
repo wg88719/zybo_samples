dd4e7fc71bba73a83f10e2c92f96898e0c97bddc
Luke Hsiao <luke.w.hsiao@gmail.com>
2015-05-05 21:08:49 -0600

    Added macros and clockDisplay_init() for testing

--- Consolidated_330_SW/src/Clock/clockDisplay.c
+++ Consolidated_330_SW/src/Clock/clockDisplay.c
@@ -14,8 +14,8 @@ uint8_t hours;  // track hours
 uint8_t minutes;  // track minutes
 uint8_t seconds;  // track seconds
 
-char[] current_time;  // char array of digits representing the current time
-char[] previous_time; // char array of digits representing the previous time
+char current_time[NUM_CHARS];  // char array representing the current time
+char previous_time[NUM_CHARS]; // char array representing the previous time
 
 
 //*****************************************************************************
@@ -23,17 +23,17 @@ char[] previous_time; // char array of digits representing the previous time
 
 /**
  * The touchscreen is divided into 6 rectangular regions numbered 0-5 as shown:
- *    +--+---+---+---+--+
- *    |  |   |   |   |  |
- *    +--+---+---+---+--+
- *    |  | 0 | 1 | 2 |  |
- *    +--+---+---+---+--+
- *    |  | H | M | S |  |
- *    +--+---+---+---+--+
- *    |  | 3 | 4 | 5 |  |
- *    +--+---+---+---+--+
- *    |  |   |   |   |  |
- *    +--+---+---+---+--+
+ * +--+---+---+---+---+---+---+---+---+--+
+ * |  |   |   |   |   |   |   |   |   |  |
+ * +--+===+===+===+===+===+===+===+===+--+
+ * |  |   0   |   |   1   |   |   2   |  |
+ * +--+---+---+---+---+---+---+---+---+--+
+ * |  | H | H | : | M | M | : | S | S |  |
+ * +--+---+---+---+---+---+---+---+---+--+
+ * |  |   3   |   |   4   |   |   5   |  |
+ * +--+===+===+===+===+===+===+===+===+--+
+ * |  |   |   |   |   |   |   |   |   |  |
+ * +--+---+---+---+---+---+---+---+---+--+
  * where each region contains a rectangle. This function returns which region
  * was pressed by the user.
  * @param  x The x-coordinate of the point pressed by the user
@@ -43,43 +43,87 @@ char[] previous_time; // char array of digits representing the previous time
 uint8_t clockDisplay_getInputRegion(int16_t x, int16_t y) {
 
 }
-
-
 //********************** End Helper Functions *********************************
 
 
 void clockDisplay_init() {
   display_init(); // Initialize the LCD display
-  display_fillScreen(DISPALY_BLACK);  // Blank the screen
+  display_fillScreen(DISPLAY_BLACK);  // Blank the screen
 
+  //////////////////////////////////////////////////////////////
   // Draw all components of the display that do not change:
+  //////////////////////////////////////////////////////////////
+
   // Draw all of the user-input triangles:
+
   // Draw Hour Up Triangle
-  display_fillTriangle( COLUMN_0,
-                        ROW_2-BUFFER,
-                        COLUMN_1,
-                        ROW_3,
-                        COLUMN_2,
-                        ROW_2-BUFFER,
-                        DISPLAY_GREEN);
+  display_fillTriangle( COLUMN_0,               // x0
+                        ROW_1 - BUFFER,         // y0
+                        COLUMN_2,               // x1
+                        ROW_1 - BUFFER,         // y1
+                        COLUMN_1,               // x2
+                        ROW_0 + ARROW_HEIGHT,   // y2
+                        DISPLAY_GREEN);         // color
 
   // Draw Minute Up Triangle
+  display_fillTriangle( COLUMN_3,               // x0
+                        ROW_1 - BUFFER,         // y0
+                        COLUMN_5,               // x1
+                        ROW_1 - BUFFER,         // y1
+                        COLUMN_4,               // x2
+                        ROW_0 + ARROW_HEIGHT,   // y2
+                        DISPLAY_GREEN);         // color
 
   // Draw Second Up Triangle
+  display_fillTriangle( COLUMN_6,               // x0
+                        ROW_1 - BUFFER,         // y0
+                        COLUMN_8,               // x1
+                        ROW_1 - BUFFER,         // y1
+                        COLUMN_5,               // x2
+                        ROW_0 + ARROW_HEIGHT,   // y2
+                        DISPLAY_GREEN);         // color
 
   // Draw Hours Down Triangle
+  display_fillTriangle( COLUMN_0,               // x0
+                        ROW_2 + BUFFER,         // y0
+                        COLUMN_2,               // x1
+                        ROW_2 + BUFFER,         // y1
+                        COLUMN_1,               // x2
+                        ROW_3 - ARROW_HEIGHT,   // y2
+                        DISPLAY_GREEN);         // color
 
   // Draw Minute Down Triangle
+  display_fillTriangle( COLUMN_3,               // x0
+                        ROW_2 + BUFFER,         // y0
+                        COLUMN_5,               // x1
+                        ROW_1 - BUFFER,         // y1
+                        COLUMN_4,               // x2
+                        ROW_3 - ARROW_HEIGHT,   // y2
+                        DISPLAY_GREEN);         // color
 
   // Draw Second Down Triangle
+  display_fillTriangle( COLUMN_6,               // x0
+                        ROW_2 + BUFFER,         // y0
+                        COLUMN_8,               // x1
+                        ROW_2 + BUFFER,         // y1
+                        COLUMN_5,               // x2
+                        ROW_3 - ARROW_HEIGHT,   // y2
+                        DISPLAY_GREEN);         // color
 
   // Draw the Clock Character ':' Separators
-  display_drawChar( COLUMN_2,
-                    ROW_2+TEXT_BLOCK_CENTER_HEIGHT,
-                    ':',
-                    DISPLAY_GREEN,
-                    DISPLAY_BLACK,
-                    SCALE_FACTOR);
+  display_drawChar( COLUMN_2,                   // x
+                    ROW_1,                      // y
+                    ':',                        // char to draw
+                    DISPLAY_GREEN,              // color of text
+                    DISPLAY_BLACK,              // color of background
+                    CLOCK_TEXT_SIZE);           // size of text
+
+  display_drawChar( COLUMN_5,                   // x
+                    ROW_1,                      // y
+                    ':',                        // char to draw
+                    DISPLAY_GREEN,              // color of text
+                    DISPLAY_BLACK,              // color of background
+                    CLOCK_TEXT_SIZE);           // size of text
 }
 
 void clockDisplay_updateTimeDisplay(bool forceUpdateAll) {
--- Consolidated_330_SW/src/Clock/clockDisplay.h
+++ Consolidated_330_SW/src/Clock/clockDisplay.h
@@ -6,17 +6,17 @@
 // Details:
 //    Note that the area of the touchscreen will be displayed and scaled usering
 //    these regions:
-//    +--+---+---+---+--+
-//    |  |   |   |   |  |
-//    +--+---+---+---+--+
-//    |  | ▲ | ▲ | ▲ |  |
-//    +--+---+---+---+--+
-//    |  | H | M | S |  |
-//    +--+---+---+---+--+
-//    |  | ▼ | ▼ | ▼ |  |
-//    +--+---+---+---+--+
-//    |  |   |   |   |  |
-//    +--+---+---+---+--+
+// +--+---+---+---+---+---+---+---+---+--+
+// |  |   |   |   |   |   |   |   |   |  |
+// +--+===+===+===+===+===+===+===+===+--+
+// |  |   ▲   |   |   ▲   |   |   ▲   |  |
+// +--+---+---+---+---+---+---+---+---+--+
+// |  | H | H | : | M | M | : | S | S |  |
+// +--+---+---+---+---+---+---+---+---+--+
+// |  |   ▼   |   |   ▼   |   |   ▼   |  |
+// +--+===+===+===+===+===+===+===+===+--+
+// |  |   |   |   |   |   |   |   |   |  |
+// +--+---+---+---+---+---+---+---+---+--+
 //*****************************************************************************
 
 #include <stdbool.h>
@@ -42,34 +42,70 @@
 #define REGION_4  4
 #define REGION_5  5
 
+// Constants for calculating time rollovers
+#define MAX_SECS  60
+#define MAX_MINS  60
+#define MAX_HRS   12  // Since we're displaying a 12-hr clock
 
 //*****************************************************************************
 // Because the size of the arrows and clock characters are scaled based on
 // CLOCK_TEXT_SIZE, the macros below perform all of the necessary scaling
 // calculations at compile time.
 
-// These macros specify the position of the 8 clock characters (HH:MM:SS)
-#define COLUMN_0 CURSOR_START_WIDTH                         // Left edge
-#define COLUMN_1 CURSOR_START_WIDTH + (TEXT_BLOCK_WIDTH)    // 1 column over
-#define COLUMN_2 CURSOR_START_WIDTH + (TEXT_BLOCK_WIDTH*2)	// 2 columns over
-#define COLUMN_3 CURSOR_START_WIDTH + (TEXT_BLOCK_WIDTH*3)	// 3 columns over
-#define COLUMN_4 CURSOR_START_WIDTH + (TEXT_BLOCK_WIDTH*4)	// 4 columns over
-#define COLUMN_5 CURSOR_START_WIDTH + (TEXT_BLOCK_WIDTH*5)	// 5 columns over
-#define COLUMN_6 CURSOR_START_WIDTH + (TEXT_BLOCK_WIDTH*6)	// 6 columns over
-#define COLUMN_7 CURSOR_START_WIDTH + (TEXT_BLOCK_WIDTH*7)	// 7 columns over
-#define COLUMN_8 CURSOR_START_WIDTH + (TEXT_BLOCK_WIDTH*8)	// 8 columns over
+#define CLOCK_TEXT_MAX 6  // The maximum value CLOCK_TEXT_SIZE may take
 
-// Unscaled Values based on screen size
-#define BOX_HEIGHT  display_height() / 5
-#define BOX_WIDTH   display_width() / 5
+// Scaled Values based on screen size. In other words, the largest the clock
+// will be drawn is as if the full screen was available.
+// For example: if CLOCK_TEXT_SIZE = 6, then
+//    OUTER_BOX_HEIGHT = display_height() * 6 / 6 = display_height()
+// which represents the largest value OUTER_BOX_HEIGHT will take.
 
-// Screen Position Macros
-#define FOURTH(X)         ((X)/4) // Divide the given number by 4
-#define HALF(X)           ((X)/2) // Divide the given number by 2
-#define THREE_FOURTHS(X)  (((X) * 3)/4) // Multiply by 3, then divide by 4
+// OUTER_BOX_HEIGHT and OUTER_BOX_WIDTH represent the dimensions of the box to
+// draw the clock in.
+#define OUTER_BOX_HEIGHT ((display_height()*CLOCK_TEXT_SIZE)/(CLOCK_TEXT_MAX))
+#define OUTER_BOX_WIDTH  ((display_width()*CLOCK_TEXT_SIZE)/(CLOCK_TEXT_MAX))
+
+// These marginal values represent the dimenion to leave blank on each edge
+// when drawing the clock.
+#define HEIGHT_MARGIN ((display_height() - (OUTER_BOX_HEIGHT)) / 2)
+#define WIDTH_MARGIN  ((display_width() - (OUTER_BOX_WIDTH)) / 2)
+
+// BUFFER represents the spacing between the drawn characters/arrows and the
+// edges of their designated boxes.
+#define BUFFER  (CLOCK_TEXT_SIZE * 2)
+
+// ORIGIN values represent the top-left corner of the scaled box. Arrows and
+// clock characters will be drawn with respect to this origin.
+#define ORIGIN_X  (WIDTH_MARGIN)
+#define ORIGIN_Y  (HEIGHT_MARGIN)
+
+// These macros define the dimension of the sub-boxes in which arrows and
+// characters will be drawn. The OUTER_BOX will be segmented 3x3 into rectangles
+#define SUB_BOX_HEIGHT  ((OUTER_BOX_HEIGHT) / 3)
+#define SUB_BOX_WIDTH   ((OUTER_BOX_WIDTH) / 8)
+#define ARROW_HEIGHT    ((SUB_BOX_HEIGHT) / 2)
+
+
 //*************** End Scaling Calculation Macros ******************************
 
+// These macros specify the position of the 8 clock characters (HH:MM:SS)
+#define COLUMN_0 (ORIGIN_X)                     // Left edge
+#define COLUMN_1 (ORIGIN_X + (SUB_BOX_WIDTH))   // 1 column over
+#define COLUMN_2 (ORIGIN_X + (SUB_BOX_WIDTH*2))	// 2 columns over
+#define COLUMN_3 (ORIGIN_X + (SUB_BOX_WIDTH*3))	// 3 columns over
+#define COLUMN_4 (ORIGIN_X + (SUB_BOX_WIDTH*4))	// 4 columns over
+#define COLUMN_5 (ORIGIN_X + (SUB_BOX_WIDTH*5))	// 5 columns over
+#define COLUMN_6 (ORIGIN_X + (SUB_BOX_WIDTH*6))	// 6 columns over
+#define COLUMN_7 (ORIGIN_X + (SUB_BOX_WIDTH*7))	// 7 columns over
+#define COLUMN_8 (ORIGIN_X + (SUB_BOX_WIDTH*8))	// 8 columns over
 
+// These macros specify the starting position of each row's y-value
+#define ROW_0 (ORIGIN_Y)                            // Top of the outer box
+#define ROW_1 (ORIGIN_Y + SUB_BOX_HEIGHT)           // 1 row down
+#define ROW_2 (ORIGIN_Y + (SUB_BOX_HEIGHT*2))       // 2 rows down
+#define ROW_3 (ORIGIN_Y + (SUB_BOX_HEIGHT*3))       // 3 rows down
+
+#define NUM_CHARS 8
 
 /**
  * Initializes the clock display. Should only be called once.
