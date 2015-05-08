bc4e52ab64c826aa73ca41c7f11e1c354f066d17
Luke Hsiao <luke.w.hsiao@gmail.com>
2015-05-07 21:44:18 -0600

    Added clockControl base code

--- Consolidated_330_SW/src/Clock/clockControl.h
+++ Consolidated_330_SW/src/Clock/clockControl.h
@@ -4,8 +4,14 @@
 // Interface for controlling with the clock
 //*****************************************************************************
 
+#define MS_COUNTER_WAIT   100 // wait for 100, 10ms intervals
+#define HALF_SECOND_WAIT  50  // wait for 50 10ms intervals
+#define TENTH_SECOND_WAIT 10  // wait for 10 10ms intervals
+#define ADC_WAIT          5   // wait for 50ms for ADC to settle
+
 /**
  * This tick function controls the state transitions and state actions
- * of the clock control state machine.
+ * of the clock control state machine. Note that timer interrupts should
+ * come every 10 milliseconds
  */
 void clockControl_tick();
