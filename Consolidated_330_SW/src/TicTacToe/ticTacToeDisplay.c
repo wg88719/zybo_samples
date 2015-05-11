ca5f86c5ed53495441e185dac0b60441f84d0a70
Luke Hsiao <luke.w.hsiao@gmail.com>
2015-05-10 20:25:04 -0600

    Initial commit of TicTacToe display files

---
+++ Consolidated_330_SW/src/TicTacToe/ticTacToeDisplay.c
@@ -0,0 +1,33 @@
+//*****************************************************************************
+// Luke Hsiao
+// 11 May 2015
+// Implementation for drawing Tic Tac Toe
+//*****************************************************************************
+
+#include "ticTacToeDisplay.h"
+
+
+void ticTacToeDIsplay_init() {
+
+}
+
+void ticTacToeDisplay_drawX(uint8_t row, uint8_t column) {
+
+}
+
+void ticTacToeDisplay_drawO(uint8_t row, uint8_t column) {
+
+}
+
+void ticTacToeDisplay_touchScreenComputeBoardRowColumn(uint8_t* row, uint8_t* column) {
+  display_clearOldTouchData();  // clear old data to read current coordinates
+  
+}
+
+void ticTacToeDisplay_runTest() {
+
+}
+
+void ticTacToeDisplay_drawBoardLines() {
+
+}
---
+++ Consolidated_330_SW/src/TicTacToe/ticTacToeDisplay.h
@@ -0,0 +1,61 @@
+//*****************************************************************************
+// Luke Hsiao
+// 11 May 2015
+// Interface for drawing Tic Tac Toe
+//*****************************************************************************
+
+// Contants for specifying row/column values
+#define ROW_0 0
+#define ROW_1 1
+#define ROW_2 2
+#define COL_0 0
+#define COL_1 1
+#define COL_2 2
+
+
+/**
+ * Inits the tic-tac-toe display, draws the lines that form the board.
+ */
+void ticTacToeDisplay_init();
+
+/**
+ * Draws an 'X' at the specified row and column.
+ * @param row    Row number (0, 1, or 2)
+ * @param column Column number (0, 1, or 2)
+ */
+void ticTacToeDisplay_drawX(uint8_t row, uint8_t column);
+
+/**
+ * Draws an 'O' at the specified row and column.
+ * @param row    Row number (0, 1 or 2)
+ * @param column Column number (0, 1, or 2)
+ */
+void ticTacToeDisplay_drawO(uint8_t row, uint8_t column);
+
+/**
+ * After a touch has been detected and after the proper delay, this sets the
+ * row and column arguments according to where the user touched the board.
+ * The values return will be (0, 1, or 2).
+ * @param row    Pointer to the row variable to modify
+ * @param column Pointer to the column variable to modify
+ */
+void ticTacToeDisplay_touchScreenComputeBoardRowColumn(uint8_t* row, uint8_t* column);
+
+// Runs a test of the display. Does the following.
+// Draws the board. Each time you touch one of the screen areas, the screen will paint
+// an X or an O, depending on whether switch 0 (SW0) is slid up (O) or down (X).
+// When BTN0 is pushed, the screen is cleared. The test terminates when BTN1 is pushed.
+/**
+ * Runs a test of the display by doing the following:
+ * 	1. Draws the board
+ * 	2. Each time a screen area is touched, the screen will draw an 'X' or an 'O'
+ * 		 depending on whether switch 0 is slid up ('O') or down ('X').
+ * 	3. When BTN0 is pushed, the screen is cleared.
+ * 	4. The test is terminated by pressing BTN1.
+ */
+void ticTacToeDisplay_runTest();
+
+/**
+ * Draws the four board lines.
+ */
+void ticTacToeDisplay_drawBoardLines();
